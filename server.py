db2852d12270c97e2da94177e065384d01b96e7a
Bastien Cadiot <bastien.cadiot@gmail.com>
2022-07-19 16:21:19 +0200

    fix errors

--- server.py
+++ server.py
@@ -1,35 +1,24 @@
-#!/usr/bin/python
-import subprocess,os
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 PORT_NUMBER = 80
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-  #Handler for the GET requests
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type','text/html')
-    self.end_headers()
-    # Send the html message
-    self.wfile.write("*** Python - Hello World ! ***\n")
-    self.wfile.write("WELCOME_MSG : " + os.getenv('WELCOME_MSG', 'undef') )
-    self.wfile.write("\n")
-    self.wfile.write("Hostname is : " + subprocess.check_output("uname -n", shell=True))
-    self.wfile.write("Process ID  : " + str(os.getpid()))
-    return
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
 
 try:
   #Create a web server and define the handler to manage the
   #incoming request
-  server = HTTPServer(('', PORT_NUMBER), myHandler)
-  print 'Started httpserver on port ' , PORT_NUMBER
+  server = HTTPServer(('', PORT_NUMBER), SimpleHTTPRequestHandler)
+  print('Started httpserver on port ' , PORT_NUMBER)
 
   #Wait forever for incoming htto requests
   server.serve_forever()
 
 except KeyboardInterrupt:
-  print '^C received, shutting down the web server'
+  print('^C received, shutting down the web server')
   server.socket.close()
